4133f251497671a6a4c1312fc6bac6d643b41ea9
Yinyin Liao <yinyinl@gmail.com>
2012-03-10 16:47:30 +0800

    add testing code for bitwriter

---
+++ test/test_bitwriter.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "bitwriter.h"
+
+int main(int argc, char ** argv)
+{
+	BitWriter writer;
+	int errno_val;
+	int bitcount;
+
+	char CODEMAP[62];
+
+	uint64_t v;
+
+	int i;
+
+	if(-1 == bitwriter_open("BINWRITE.out", &writer, &errno_val))
+	{
+		fprintf(stderr, "ERR: cannot open file.\n");
+		return 1;
+	}
+
+	for(i=0; i<26; i++) {
+		CODEMAP[i+0] = (char)(i+((int)('a')));
+	}
+	for(i=0; i<26; i++) {
+		CODEMAP[i+26] = (char)(i+((int)('A')));
+	}
+	for(i=0; i<10; i++) {
+		CODEMAP[i+52] = (char)(i+((int)('0')));
+	}
+
+	for(i = 0; i < 18; i++) {
+		uint64_t vl;
+
+		vl = (uint64_t)(CODEMAP[ i%62 ]);
+
+		if( -1 == (bitcount = bitwriter_write_integer_bits(&writer, vl, 8, &errno_val)) )
+		{
+			fprintf(stderr, "ERR: cannot write to file.\n");
+			return 1;
+		}
+	}
+
+
+	if( -1 == (bitcount = bitwriter_write_integer_bits(&writer, 9L, 6, &errno_val)) )
+	{
+		fprintf(stderr, "ERR: cannot write to file.\n");
+		return 1;
+	}
+
+
+	for(i = 0; i < 256; i++) {
+		uint64_t vl;
+
+		vl = (uint64_t)(CODEMAP[ i%62 ]);
+
+		if( -1 == (bitcount = bitwriter_write_integer_bits(&writer, vl, 8, &errno_val)) )
+		{
+			fprintf(stderr, "ERR: cannot write to file.\n");
+			return 1;
+		}
+	}
+
+#if 1
+	if(-1 == bitwriter_close(&writer, &errno_val))
+	{
+		fprintf(stderr, "ERR: failed on closing file.\n");
+	}
+
+#endif
+
+	return 0;
+}
